059252a8d0b9917da1a2659b8ebd1347b4be6440
Indranil Banerjee <pnl0bn9v@emea.royalahold.net>
2020-09-09 20:41:00 +0530

    htmlunitadded

--- src/test/java/com/example/sprintbootfirst/SprintBootFirstApplicationTests.java
+++ src/test/java/com/example/sprintbootfirst/SprintBootFirstApplicationTests.java
@@ -1,32 +1,39 @@
 
 package com.example.sprintbootfirst;
-import static net.sourceforge.jwebunit.junit.JWebUnit.assertLinkPresent;
-import static net.sourceforge.jwebunit.junit.JWebUnit.assertTitleEquals;
-import static net.sourceforge.jwebunit.junit.JWebUnit.beginAt;
-import static net.sourceforge.jwebunit.junit.JWebUnit.clickLink;
-import static net.sourceforge.jwebunit.junit.JWebUnit.setBaseUrl;
-import static net.sourceforge.jwebunit.junit.JWebUnit.setTestingEngineKey;
- 
+
 import org.junit.Before;
 import org.junit.Test;
  
 import net.sourceforge.jwebunit.util.TestingEngineRegistry;
 import static net.sourceforge.jwebunit.junit.JWebUnit.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 @SpringBootTest
 public class SprintBootFirstApplicationTests {
 @Before
-public void prepare() throws Exception {
-        setTestingEngineKey(TestingEngineRegistry.TESTING_ENGINE_HTMLUNIT); 
-        setBaseUrl("http://localhost:8080/login");
-    }
+public void preparePageload() throws FailingHttpStatusCodeException, MalformedURLException, IOException{
+	WebClient webClient = new WebClient();
+	HtmlPage currentPage = webClient.getPage("http://www.google.com/");
+//	HtmlPage currentPage = webClient.getPage("http://localhost:8080");
+	assertEquals("Google", currentPage.getTitleText());
+}
 
 @Test
 public void contextLoads() {
-        beginAt("login.jsp"); 
-        assertTitleEquals("Sprint Boot JSP LogIn Page");
-        setTextField("username", "Jack1234");
+	 System.out.println("Start 111");
+//	 assertEquals("Google", currentPage.getTitleText());
+  //       beginAt("login"); 
+  //      assertTitleEquals("Sprint Boot JSP LogIn Page");
+  //      setTextField("username", "Jack1234");
   //      assertLinkPresent("home");
   //      clickLink("home");
   //      assertTitleEquals("Home");
